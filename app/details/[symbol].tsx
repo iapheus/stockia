9e096c93cf667edfa30bc97cb2c2d2a762e664e2
iapheus <kodlamailealakali@gmail.com>
2025-04-20 04:08:48 +0300

    details page updated

--- app/details/[symbol].tsx
+++ app/details/[symbol].tsx
@@ -1,12 +1,181 @@
-import { useLocalSearchParams } from 'expo-router';
-import { ScrollView, StyleSheet, Text } from 'react-native';
-import { colors } from '../../constants/colors';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocalSearchParams } from 'expo-router';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Button,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function index() {
+export default function Index() {
   const { symbol } = useLocalSearchParams();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [visibleItemsCount, setVisibleItemsCount] = useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const marketCountry = await AsyncStorage.getItem('marketCountry');
+        const lang = await AsyncStorage.getItem('newsLanguage');
+        if (!marketCountry || !lang) {
+          console.error('Keys not found in AsyncStorage');
+          return;
+        }
+
+        const isCurrency = symbol.startsWith('FX_IDC');
+        const baseUrl = 'http://192.168.1.103:3000/api/v1';
+
+        const urls = isCurrency
+          ? [
+              `${baseUrl}/currency/${symbol.slice(symbol.indexOf(':') + 1)}`,
+              `${baseUrl}/currency/events/${symbol.slice(
+                symbol.indexOf(':') + 1
+              )}`,
+              `${baseUrl}/currency/performance/${symbol.slice(
+                symbol.indexOf(':') + 1
+              )}`,
+              `${baseUrl}/currency/news/${symbol.slice(
+                symbol.indexOf(':') + 1
+              )}/${lang}`,
+            ]
+          : [
+              `${baseUrl}/stock/${marketCountry}/${symbol.slice(
+                symbol.indexOf(':') + 1
+              )}`,
+              `${baseUrl}/stock/news?stockName=${symbol}`,
+              `${baseUrl}/stock/performance?stockName=${symbol}`,
+            ];
+
+        const responsePromises = urls.map((url) =>
+          fetch(url).then((res) => res.json())
+        );
+        const responses = await Promise.all(responsePromises);
+        setData(responses);
+      } catch (error) {
+        console.error('Data fetching error:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [symbol]);
+
+  const loadMore = () => {
+    setVisibleItemsCount(visibleItemsCount + 5);
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color='#1E90FF' />
+      </View>
+    );
+  }
+
   return (
     <ScrollView style={styles.container}>
-      <Text>{symbol}</Text>
+      {/* General Information */}
+      <View style={styles.dataSection}>
+        <Text style={styles.sectionTitle}>General Information</Text>
+        {data && data[0]?.data?.items ? (
+          <View style={styles.dataItem}>
+            {/* Show the first item */}
+            <View key={0}>
+              <Text style={styles.dataTitle}>{data[0].data.items[0].name}</Text>
+              <Text style={styles.dataText}>
+                {data[0].data.items[0].description}
+              </Text>
+              <Text style={styles.dataText}>
+                Close: {data[0].data.items[0].close}
+              </Text>
+              <Text style={styles.dataText}>
+                Change: {data[0].data.items[0].change.toFixed(2)}%
+              </Text>
+            </View>
+          </View>
+        ) : (
+          <Text style={{ marginBottom: 25 }}>No data available</Text>
+        )}
+      </View>
+
+      {/* Performance */}
+      <View style={styles.dataSection}>
+        <Text style={styles.sectionTitle}>Performance</Text>
+        {data && data[2]?.data ? (
+          <View style={styles.dataItem}>
+            {Object.entries(data[2].data).map(([key, value], index) => (
+              <View key={index} style={{ flexDirection: 'row', gap: 10 }}>
+                <Text style={styles.dataTitle}>{key}:</Text>
+                <Text style={styles.dataText}>{value.toFixed(2)}%</Text>
+              </View>
+            ))}
+          </View>
+        ) : (
+          <Text>No performance data available</Text>
+        )}
+      </View>
+
+      {/* Events */}
+      <View style={styles.dataSection}>
+        <Text style={styles.sectionTitle}>Events</Text>
+        {data && data[1]?.data ? (
+          <View style={styles.dataItem}>
+            {data[1].data.slice(0, visibleItemsCount).map((event, index) => (
+              <View key={index}>
+                <Text style={styles.dataTitle}>{event.title}</Text>
+                {event.urgency && (
+                  <Text style={styles.dataText}>Urgency: {event.urgency}</Text>
+                )}
+                {event.published && (
+                  <Text style={styles.dataText}>
+                    Date:{' '}
+                    {new Date(event.published * 1000).toLocaleDateString()}
+                  </Text>
+                )}
+              </View>
+            ))}
+            {data[1].data.length > visibleItemsCount && (
+              <View style={styles.loadMoreButtonContainer}>
+                <Button title='Load More' onPress={loadMore} color='black' />
+              </View>
+            )}
+          </View>
+        ) : (
+          <Text>No events available</Text>
+        )}
+      </View>
+
+      {/* News */}
+      <View style={(styles.dataSection, { paddingBottom: 75 })}>
+        <Text style={styles.sectionTitle}>News</Text>
+        {data && data[3]?.data ? (
+          <View style={styles.dataItem}>
+            {data[3].data.slice(0, visibleItemsCount).map((news, index) => (
+              <View key={index}>
+                <Link href={`https://tradingview.com${news.storyPath}`}>
+                  <Text style={styles.dataTitle}>{news.title}</Text>
+                </Link>
+                <Text style={styles.dataText}>
+                  Published:{' '}
+                  {new Date(news.published * 1000).toLocaleDateString()}
+                </Text>
+              </View>
+            ))}
+            {data[3].data.length > visibleItemsCount && (
+              <View style={styles.loadMoreButtonContainer}>
+                <Button title='Load More' onPress={loadMore} color='black' />
+              </View>
+            )}
+          </View>
+        ) : (
+          <Text>No news available</Text>
+        )}
+      </View>
     </ScrollView>
   );
 }
@@ -14,7 +183,53 @@ export default function index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 70,
-    backgroundColor: colors.secondaryDarkBG,
+    backgroundColor: '#f4f4f9',
+    paddingVertical: 75,
+    paddingHorizontal: 15,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f4f4f9',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#333',
+    marginBottom: 15,
+  },
+  dataItem: {
+    backgroundColor: '#fff',
+    padding: 18,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    marginBottom: 20,
+    gap: 20,
+  },
+  dataTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#19589b',
+    marginBottom: 8,
+  },
+  dataText: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 6,
+  },
+  loadMoreButtonContainer: {
+    marginTop: 15,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
   },
 });
